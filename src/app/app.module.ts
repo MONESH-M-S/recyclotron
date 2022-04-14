8829bd75293d9697a2c6b9883dd2d723c9f2e022
MoneshMS <moneshsanju@gmail.com>
2022-04-14 10:38:55 +0530

    Modified home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HomeAvailableScrapsComponent } from './home/home-available-scraps/home-available-scraps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    HomeAvailableScrapsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home-available-scraps/home-available-scraps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-available-scraps',
+  templateUrl: './home-available-scraps.component.html',
+  styleUrls: ['./home-available-scraps.component.scss']
+})
+export class HomeAvailableScrapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
